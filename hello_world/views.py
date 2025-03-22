89f00e23f42ef22e85c1d70f6779aa7495656627
BartłomiejKieżun <vdi-terminal@EDU-WSB.local>
2025-03-22 18:13:54 +0100

    Poprawki Liner

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,9 +2,9 @@ from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
-
 moje_imie = "Bartlomiej"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -12,8 +12,9 @@ def index():
     if not output:
         output = PLAIN
     return get_formatted(msg, moje_imie,
-                         output.lower())
-
+        output.lower())
 @app.route('/outputs')
+
+
 def supported_output():
     return ", ".join(SUPPORTED)
