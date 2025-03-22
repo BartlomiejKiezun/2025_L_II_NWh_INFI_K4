24df2cd423817a69c9ab6e168da83af8c114397d
BartłomiejKieżun <vdi-terminal@EDU-WSB.local>
2025-03-22 17:59:13 +0100

    stworzenie aliasów w makefile

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Bartłomiej", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Bartlomiej", "msg":"Hello World!"}', rv.data)
